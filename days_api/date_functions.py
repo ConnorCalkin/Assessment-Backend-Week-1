57ffb063b6df03c6c807a040384b360d78706422
connorcalkin <connor_calkin@outlook.com>
2026-01-16 14:22:45 +0000

    Added date conversion and date difference functions

--- days_api/date_functions.py
+++ days_api/date_functions.py
@@ -4,11 +4,15 @@ from datetime import datetime, date
 
 
 def convert_to_datetime(date_val: str) -> datetime:
-    pass
+    '''converts string of format DAY.MONTH.YEAR to a datetime object'''
+    try:
+        return datetime.strptime(date_val, "%d.%-m.%Y")
+    except ValueError as e:
+        raise ValueError("Unable to convert value to datetime.") from e
 
 
 def get_days_between(first: datetime, last: datetime) -> int:
-    pass
+    return (last - first).days
 
 
 def get_day_of_week_on(date_val: datetime) -> str:
